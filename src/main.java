492278cb25ea3cf5971450cc4e631b84909ff58b
Маргарита Мусатова <musatovamargarita@gmail.com>
2022-06-21 21:14:03 +0300

    создана программа для вычисления миль

---
+++ src/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String... args) {
+        int ticket = 10_000;
+        int miles = 20;
+        int bonus = ticket/miles;
+        System.out.println(" За данный билет будет начислено миль: " + bonus);
+    }
+}
