9f85453d0747cd887ae548597388522b4f62e87d
Маргарита Мусатова <musatovamargarita@gmail.com>
2022-06-22 12:36:58 +0300

    Кот отформатирован

--- src/main.java
+++
@@ -1,8 +0,0 @@
-public class main {
-    public static void main(String... args) {
-        int ticket = 10_000;
-        int miles = 20;
-        int bonus = ticket/miles;
-        System.out.println(" За данный билет будет начислено миль: " + bonus);
-    }
-}
