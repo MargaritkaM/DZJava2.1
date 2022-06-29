ddc2f4d44317acd939d4a6a3014c01db0df30bc1
Маргарита Мусатова <musatovamargarita@gmail.com>
2022-06-29 10:51:56 +0300

    Добавлена функция

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
-    public static void main(String... args) {
-        int ticket = 10_000;
-        int miles = 20;
-        int bonus = ticket / miles;
-        System.out.println("За данный билет будет начислено миль: " + bonus);
+    public static void main(String[] args) {
+        BonusMilesService service = new BonusMilesService();
+        int price = 30_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
